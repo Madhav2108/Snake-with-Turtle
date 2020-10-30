68737b13c83873437738e96b3c882d6a37b69f34
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-31 02:42:51 +0530

    Add files via upload

--- src.py
+++ src.py
@@ -3,3 +3,12 @@ import time
 import random
 
 delay = 0.1
+
+score=0
+high_score=0
+
+wn=turtle.Screen()
+wn.title("Snake Game by Mahesh Sawant")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
