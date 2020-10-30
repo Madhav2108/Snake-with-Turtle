d0379943d1bffb3fdee737bf981865366201b51a
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-31 02:43:51 +0530

    Add files via upload

--- src.py
+++ src.py
@@ -12,3 +12,19 @@ wn.title("Snake Game by Mahesh Sawant")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0)
+
+# head
+head=turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction="stop"
+# khana
+food=turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
